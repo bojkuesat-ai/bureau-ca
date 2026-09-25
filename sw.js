715ae029755ff7bbd071412485c84831c3566abc
bojkuesat-ai <bojkuesat@gmail.com>
2026-09-25 20:51:44 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@
    Le changement de nom déclenche la purge de l'ancien cache dans "activate",
    donc plus aucune vieille version ne peut rester coincée. */
 
-const CACHE = "ca-v23";
+const CACHE = "ca-v25";
 
 self.addEventListener("install", () => self.skipWaiting());
 
